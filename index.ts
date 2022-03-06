b6e3dbe699fc75b0d750cb81ea110ec4008cdb2d
el131313 <elitsaleontieva@abv.bg>
2022-03-06 18:25:36 +0200

    1 file modified

--- index.ts
+++ index.ts
@@ -4,3 +4,29 @@ import './style.css';
 // Write TypeScript code!
 const appDiv: HTMLElement = document.getElementById('app');
 appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
+
+class Request{
+  method: string;
+  uri: string;
+  version: string;
+  message: string;
+  response:string = undefined;
+  fulfiller: boolean = false;
+
+  constructor(method: string, uri: string, version:string, message: string){
+    this.method = "323";
+    this.uri = "323"
+    this.version = "this.version3";
+    this.message = "this.message3";
+  }
+}
+
+let myData = new Request('GET', 'http://google.com', 'HTTP/1.1', '');
+appDiv.innerHTML += '<h1>'+myData.method+'<h1>';
+appDiv.innerHTML += '<h1>'+myData.uri+'<h1>';
+appDiv.innerHTML += '<h1>'+myData.version+'<h1>';
+appDiv.innerHTML += '<h1>'+myData.message+'<h1>';
+appDiv.innerHTML += '<h1>'+myData.fulfiller+'<h1>';
+appDiv.innerHTML += '<h1>'+myData.response+'<h1>';
+
+
